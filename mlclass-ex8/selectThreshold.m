50e84ab44fa2ef8841683ff459737d6431224090
Polina Giralt <polina.giralt@gmail.com>
2013-12-17 15:55:23 -0500

    Find the best threshold (epsilon) to use for selecting outliers

--- mlclass-ex8/selectThreshold.m
+++ mlclass-ex8/selectThreshold.m
@@ -24,14 +24,16 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
 
+    % cvPredictions is a binary vector of 0's and 1's of the outlier predictions
+    cvPredictions = (pval < epsilon);
+    fp = sum((cvPredictions == 1) & (yval == 0));
+    tp = sum((cvPredictions == 1) & (yval == 1));
+    fn = sum((cvPredictions == 0) & (yval == 1));
 
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
 
-
-
-
-
-
-
+    F1 = ((2 * prec * rec)/(prec + rec));
 
 
 
